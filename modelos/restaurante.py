09aaa5532a05381ed26c406b7b89e5b978ff1bec
dev-limatheus <souzalima1517@gmail.com>
2026-04-01 19:19:49 -0300

    refatora a classe Restaurante para inicializar atributos no construtor e simplifica a listagem de restaurantes

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,33 +1,21 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Bistrô'
-restaurante_praca.categoria = 'Italiana'
+    restaurantes = []
 
-if restaurante_praca.ativo:
-    print(f'Restaurante {restaurante_praca.nome} está inativo.')
-else:
-    print(f'Restaurante {restaurante_praca.nome} está ativo.')
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
+    
+    def __str__(self):  #transformar o endereço de memoria em texto
+        return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
-categoria = Restaurante.categoria
+restaurante_praca = Restaurante('Bistrô', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Place', 'Italiano')
 
-
-restaurante_pizza = Restaurante()
-restaurante_pizza.nome = 'Pizza Place'
-restaurante_pizza.categoria = 'Fast Food'
-
-if restaurante_pizza.categoria == 'Fast Food':
-    print(f'Categoria do {restaurante_pizza.nome} é "Fast Food".')
-else:
-    print(f'Categoria do {restaurante_pizza.nome} não é "Fast Food"')
-
-restaurante_pizza.ativo = True
-
-nome_do_restaurante = restaurante_praca.nome
-
-restaurantes = [restaurante_praca, restaurante_pizza]
-
-print(f'Nome: {restaurante_praca.nome}, Categoria: {restaurante_praca.categoria}')
+Restaurante.listar_restaurantes()
