e02874c2f160e8328b7087508629599144099f8f
dev-limatheus <souzalima1517@gmail.com>
2026-04-24 00:31:42 -0300

    adiciona métodos de desconto e refatora classes de cardápio

--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -1,9 +1,14 @@
 from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Prato(ItemCardapio):
-    def __init__(self,nome,preco,descricao):
+    def __init__(self,nome,preco,descricao, tipo, tamanho):
         super().__init__(nome,preco)
-        self._descricao = descricao
+        self.descricao = descricao
+        self.tipo = tipo
+        self.tamanho = tamanho
 
     def __str__(self):
-        return self._descricao
+        return self._nome 
+    
+    def aplicar_desconto(self):
+        self._preco -= (self._preco * 0.15)
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -17,7 +17,7 @@ class Restaurante:
     
     @classmethod
     def listar_restaurantes(cls): 
-        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | {'Visibilidade'}')
+        print(f"{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | {'Visibilidade'}")
         for restaurante in cls.restaurantes:
             print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante._ativo}')
 
